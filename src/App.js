c24e096c578294e90a38fad484ecdcf759ccfd77
Timbo <mcloha@gmail.com>
2023-01-28 10:36:36 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import { variant } from 'styled-system'
+
+const Root = styled.div``
+
+const Header = styled.header`
+    padding: var(--padding-vertical) var(--padding-horizontal);
+    background: var(--brand);
+    color: var(--white);
+`
+
+const Title = styled.h1``
+
+const Content = styled.div`
+    padding: var(--padding-vertical) var(--padding-horizontal);
+`
+
+const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    background: var(--off-white);
+    padding: var(--padding-vertical) var(--padding-horizontal);
+    border-radius: 0.5em;
+    color: var(--dark-not-black);
+`
+
+Card.Title = styled.h2``
+
+Card.Content = styled.div``
+
+const Form = styled.form``
+
+const Button = styled('button')(
+    {
+        cursor: 'pointer',
+        fontWeight: 500,
+        padding: 'var(--padding-vertical) var(--padding-horizontal)',
+        borderRadius: '0.5em',
+    },
+    variant({
+        variants: {
+            primary: {
+                backgroundColor: 'var(--brand)',
+                color: 'var(--white)',
+                '&:hover': {
+                    backgroundColor: 'var(--text-link)',
+                    transitionDuration: '0.3s',
+                },
+            },
+            secondary: {
+                backgroundColor: 'var(--not-quite-black)',
+                color: 'var(--white)',
+                '&:hover': {
+                    backgroundColor: 'var(--dark-not-black)',
+                    color: 'var(--greyple)',
+                    transitionDuration: '0.3s',
+                },
+            },
+        },
+    })
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Root>
+            <Header>
+                <Title>Header</Title>
+            </Header>
+            <Content>
+                <Card>
+                    <Card.Title>title</Card.Title>
+                    <Card.Content>
+                        <Button variant="primary">button</Button>
+                        <Button variant="secondary">button</Button>
+                    </Card.Content>
+                </Card>
+            </Content>
+        </Root>
+    )
 }
 
-export default App;
+export default App
